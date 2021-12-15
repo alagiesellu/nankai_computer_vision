0b32c962ff69a447a4fa6e805ce3e8d00e84ae0f
Alagie Sellu <alagiesellu@gmail.com>
2021-12-15 21:30:53 +0000

    Project done. But need code optimization.

--- seam_carving/main.cpp
+++ seam_carving/main.cpp
@@ -31,8 +31,8 @@ vector<vector<vector<int>>> PIXELS;
 vector<vector<int>> PIXELS_ENERGY;
 uint8_t* OUTPUT_IMAGE;
 
-vector<vector<int>> NEXT_STEP_MEMORY;
-vector<vector<int>> PATH_ENERGY_MEMORY;
+vector<vector<vector<int>>> NEXT_STEP_MEMORY;
+vector<vector<vector<int>>> PATH_ENERGY_MEMORY;
 vector<int> CARVED_PATH;
 
 void write_image(char const *filename, int& col, int&row, const int& req_comp)
@@ -109,47 +109,47 @@ int get_cheapest_next_col(int col, int next_row) {
 }
 
 int
-calculate_path_energy_up(int col, int row) {
+calculate_path_energy_up(int col, int row, const int search_depth) {
 
     if (row == 0)
         return PIXELS_ENERGY[row][col];
 
-    if (PATH_ENERGY_MEMORY[row][col] != NULL_INT_VALUE) {
-        return PATH_ENERGY_MEMORY[row][col];
+    if (PATH_ENERGY_MEMORY[row][col][search_depth] != NULL_INT_VALUE) {
+        return PATH_ENERGY_MEMORY[row][col][search_depth];
     }
 
     int next_row = row - 1;
 
-    NEXT_STEP_MEMORY[row][col] = get_cheapest_next_col(col, next_row);
+    NEXT_STEP_MEMORY[row][col][search_depth] = get_cheapest_next_col(col, next_row);
 
-    PATH_ENERGY_MEMORY[row][col] = PIXELS_ENERGY[row][col] + calculate_path_energy_up(NEXT_STEP_MEMORY[row][col], next_row);
+    PATH_ENERGY_MEMORY[row][col][search_depth] = PIXELS_ENERGY[row][col] + calculate_path_energy_up(NEXT_STEP_MEMORY[row][col][search_depth], next_row, search_depth);
 
-    return PATH_ENERGY_MEMORY[row][col];
+    return PATH_ENERGY_MEMORY[row][col][search_depth];
 }
 
 int
-calculate_path_energy_down(int col, int row) {
+calculate_path_energy_down(int col, int row, const int search_depth) {
 
     int next_row = row + 1;
 
     if (next_row == HEIGHT)
         return PIXELS_ENERGY[row][col];
 
-    if (PATH_ENERGY_MEMORY[row][col] != NULL_INT_VALUE) {
-        return PATH_ENERGY_MEMORY[row][col];
+    if (PATH_ENERGY_MEMORY[row][col][search_depth] != NULL_INT_VALUE) {
+        return PATH_ENERGY_MEMORY[row][col][search_depth];
     }
 
-    NEXT_STEP_MEMORY[row][col] = get_cheapest_next_col(col, next_row);
+    NEXT_STEP_MEMORY[row][col][search_depth] = get_cheapest_next_col(col, next_row);
 
-    PATH_ENERGY_MEMORY[row][col] = PIXELS_ENERGY[row][col] + calculate_path_energy_down(NEXT_STEP_MEMORY[row][col], next_row);
+    PATH_ENERGY_MEMORY[row][col][search_depth] = PIXELS_ENERGY[row][col] + calculate_path_energy_down(NEXT_STEP_MEMORY[row][col][search_depth], next_row, search_depth);
 
-    return PATH_ENERGY_MEMORY[row][col];
+    return PATH_ENERGY_MEMORY[row][col][search_depth];
 }
 
 int
-calculate_path_energy(int col, int row) {
+calculate_path_energy(int col, int row, const int search_depth) {
 
-    return calculate_path_energy_up(col, row - 1) + calculate_path_energy_down(col, row);
+    return calculate_path_energy_up(col, row - 1, search_depth) + calculate_path_energy_down(col, row, search_depth);
 }
 
 void remove_2d_element(vector<vector<int>> &list, int col, int row) {
@@ -160,11 +160,11 @@ void remove_3d_element(vector<vector<vector<int>>> &list, int col, int row) {
     list[row].erase(list[row].begin() + col);
 }
 
-void remove_pixel(int &cheapest_col, int row) {
+void remove_pixel(int &cheapest_col, int row, const int search_depth) {
 
     int remove_col = static_cast<int>(cheapest_col);
 
-    cheapest_col = NEXT_STEP_MEMORY[row][remove_col];
+    cheapest_col = NEXT_STEP_MEMORY[row][remove_col][search_depth];
 
     remove_3d_element(PIXELS, remove_col, row);
 
@@ -196,15 +196,15 @@ void clean_carved_path() {
     }
 }
 
-void eliminate_path(int cheapest_col, int cheapest_base_height) {
+void eliminate_path(int cheapest_col, int cheapest_base_height, const int search_depth) {
 
-    int cheapest_col_up = NEXT_STEP_MEMORY[cheapest_base_height][cheapest_col];
+    int cheapest_col_up = NEXT_STEP_MEMORY[cheapest_base_height][cheapest_col][search_depth];
     for (int row = cheapest_base_height; row < HEIGHT; row++) {
-        remove_pixel(cheapest_col, row);
+        remove_pixel(cheapest_col, row, search_depth);
     }
 
     for (int row = cheapest_base_height - 1; row >= 0; row--) {
-        remove_pixel(cheapest_col_up, row);
+        remove_pixel(cheapest_col_up, row, search_depth);
     }
 
     WIDTH--;
@@ -227,7 +227,7 @@ void carve_image_width(const int search_depth) {
 
     int next_width = calculate_next_width();
 
-    int path_energy, cheapest_col, cheapest_path_energy, cheapest_base_height;
+    int path_energy, cheapest_col, cheapest_path_energy, cheapest_base_height, cheapest_temp_search_depth;
 
     CARVED_PATH = vector<int>(HEIGHT);
 
@@ -240,40 +240,42 @@ void carve_image_width(const int search_depth) {
 
         temp_search_depth = search_depth;
 
+        PATH_ENERGY_MEMORY = vector<vector<vector<int>>>(
+                HEIGHT, vector<vector<int>>(WIDTH, vector<int>(search_depth, NULL_INT_VALUE))
+        );
+
+        NEXT_STEP_MEMORY = vector<vector<vector<int>>>(
+                HEIGHT, vector<vector<int>>(WIDTH, vector<int>(search_depth, NULL_INT_VALUE))
+        );
+
         while (temp_search_depth > 0) {
             base_height = search_height_chunks * temp_search_depth;
-
-            PATH_ENERGY_MEMORY = vector<vector<int>>(
-                    HEIGHT, vector<int>(WIDTH, NULL_INT_VALUE)
-            );
-
-            NEXT_STEP_MEMORY = vector<vector<int>>(
-                    HEIGHT, vector<int>(WIDTH, NULL_INT_VALUE)
-            );
 
             start_col = 0;
             if (first_search) {
                 cheapest_col = 0,
                 cheapest_base_height = base_height,
-                cheapest_path_energy = calculate_path_energy(0, base_height);
+                cheapest_path_energy = calculate_path_energy(0, base_height, temp_search_depth - 1);
+                cheapest_temp_search_depth = temp_search_depth - 1;
                 start_col = 1;
                 first_search = false;
             }
 
             for (int col = start_col; col < WIDTH; col++) {
-                path_energy = calculate_path_energy(col, base_height);
+                path_energy = calculate_path_energy(col, base_height, temp_search_depth - 1);
 
                 if (path_energy < cheapest_path_energy) {
                     cheapest_path_energy = path_energy;
                     cheapest_col = col;
                     cheapest_base_height = base_height;
+                    cheapest_temp_search_depth = temp_search_depth - 1;
                 }
             }
 
             temp_search_depth--;
         }
 
-        eliminate_path(cheapest_col, cheapest_base_height);
+        eliminate_path(cheapest_col, cheapest_base_height, cheapest_temp_search_depth);
     }
 }
 
@@ -360,19 +362,19 @@ int main()
 {
     WIDTH_PERCENTAGE_TO_REDUCE = 25;
 
-    seam_carve(0, JPG_EXTENSION);
-    seam_carve(1, JPG_EXTENSION);
-    seam_carve(2, JPG_EXTENSION);
-    seam_carve(3, JPG_EXTENSION);
-    seam_carve(4, JPG_EXTENSION);
-    seam_carve(5, JPG_EXTENSION);
-    seam_carve(6, JPG_EXTENSION);
-    seam_carve(7, JPG_EXTENSION);
-    seam_carve(8, JPG_EXTENSION);
-    seam_carve(9, JPG_EXTENSION);
-    seam_carve(10, JPG_EXTENSION);
-    seam_carve(11, JPG_EXTENSION);
-    seam_carve(12, JPG_EXTENSION);
+    seam_carve(0, JPG_EXTENSION, 2);
+    seam_carve(1, JPG_EXTENSION, 2);
+    seam_carve(2, JPG_EXTENSION, 2);
+    seam_carve(3, JPG_EXTENSION, 2);
+    seam_carve(4, JPG_EXTENSION, 2);
+//    seam_carve(5, JPG_EXTENSION);
+//    seam_carve(6, JPG_EXTENSION);
+//    seam_carve(7, JPG_EXTENSION);
+//    seam_carve(8, JPG_EXTENSION);
+//    seam_carve(9, JPG_EXTENSION);
+//    seam_carve(10, JPG_EXTENSION);
+//    seam_carve(11, JPG_EXTENSION);
+//    seam_carve(12, JPG_EXTENSION);
 
     return 0;
 }
